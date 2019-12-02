15f6782c2a23ffab2521f8a1a057556155f51240
zhenya_subbota <zhenya.subbota.19@mail.ru>
2019-12-02 19:35:31 +0300

    second task

--- template/urls.py
+++ template/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from template.views import echo, filters, extend
 
 urlpatterns = [
-    url(r'^echo/$', echo),
+    url(r'^echo/', echo),
     url(r'^filters/$', filters),
     url(r'^extend/$', extend),
 ]
